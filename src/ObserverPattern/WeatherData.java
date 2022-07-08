2650860e793a0020324fe39b7bec797fab72111d
Sulekha Dahiya <Sulekha.Dahiya@mvc.nj.gov>
2022-07-08 15:39:05 -0400

    7/8 Observer Pattern (Subject & Observer).

---
+++ src/ObserverPattern/WeatherData.java
@@ -0,0 +1,21 @@
+package ObserverPattern;
+
+//Publishers(Subject) + Subscribers(Observer) = Observer Pattern
+/*
+1. The Subject object manages some important data.
+2. The Observer have subscribed to (registered with) the Subject
+   to receive updates when the Subject's data changes.
+3. When data in the Subject changes, the observers are notified.
+4. New data values are communicated to the observer in some form when they change.
+ */
+// OBSERVER PATTERN : Defines a one-to-many relationship between a set of objects so that when
+// one object changes state, all of its dependents are notified and updated automatically.
+/*
+1. One subject, who notifies many observers when something in the subject changes.
+2. The observers are dependent on the subject.
+3. When the subject's state changes, observers are notified.
+ */
+
+public class WeatherData {
+
+}
